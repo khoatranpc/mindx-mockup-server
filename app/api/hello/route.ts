af205fc4cc5795d5bbf4daf6b12a1453609ef727
khoatranpc <khoatranpc603@gmail.com>
2024-12-31 17:52:24 +0700

    fix enslint

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,10 +1,10 @@
-export async function GET(request: Request) {
+export async function GET(_: Request) {
     try {
         return new Response(JSON.stringify({
             message: 'Hello MindX-er',
             data: [1, 2, 3, 4, 5, 6],
         }), { status: 200 });
-    } catch (error) {
+    } catch (error: any) {
         return new Response(JSON.stringify({
             message: 'Some errors!',
             data: null,
